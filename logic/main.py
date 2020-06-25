d94f20e8c2ed4200bffb9124dd8a09c17c451137
mihirshrestha <mihir.xtha@gmail.com>
2020-06-25 00:18:50 -0400

    added django

--- logic/main.py
+++ logic/main.py
@@ -1,4 +1,4 @@
-from trader import Trader
+from logic.trader import Trader
 
 trader1 = Trader()
 print(trader1.get_ema(sma_days=1, ema_days=20, parameter='close'))
---
+++ trading/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TradingConfig(AppConfig):
+    name = 'trading'
