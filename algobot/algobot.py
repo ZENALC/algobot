c558d342d1c80b8abbdfc3bcd81f8db098e279f6
Mihir Shrestha <mihir.xtha@gmail.com>
2020-10-19 01:02:51 -0400

    split files

--- algobot/algobot.py
+++ algobot/algobot.py
@@ -4,6 +4,8 @@ import traceback
 import assets
 
 from data import Data
+from threadWorkers import Worker
+from palettes import *
 from telegramBot import TelegramBot
 from realtrader import RealTrader
 from simulationtrader import SimulatedTrader
@@ -24,54 +26,6 @@ configurationUi = f'../UI{os.path.sep}configuration.ui'
 otherCommandsUi = f'../UI{os.path.sep}otherCommands.ui'
 statisticsUi = f'../UI{os.path.sep}statistics.ui'
 aboutUi = f'../UI{os.path.sep}about.ui'
-
-
-class WorkerSignals(QObject):
-    """
-    Defines the signals available from a running worker thread.
-    """
-    finished = pyqtSignal()
-    error = pyqtSignal(str)
-    result = pyqtSignal(object)
-    progress = pyqtSignal(int)
-
-
-class Worker(QRunnable):
-    """
-    Worker thread
-
-    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
-
-    :param callback: The function callback to run on this worker thread. Supplied args and
-                     kwargs will be passed through to the runner.
-    :type callback: function
-    :param args: Arguments to pass to the callback function
-    :param kwargs: Keywords to pass to the callback function
-
-    """
-
-    def __init__(self, fn, *args, **kwargs):
-        super(Worker, self).__init__()
-
-        # Store constructor arguments (re-used for processing)
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-        self.signals = WorkerSignals()
-
-    @pyqtSlot()
-    def run(self):
-        """
-        Initialise the runner function with passed args, kwargs.
-        """
-
-        # Retrieve args/kwargs here; and fire processing using them
-        try:
-            self.fn(*self.args, **self.kwargs)
-        except Exception as e:
-            print(f'Error: {e}')
-            traceback.print_exc()
-            self.signals.error.emit(str(e))
 
 
 class Interface(QMainWindow):
@@ -1011,93 +965,6 @@ class About(QDialog):
     def __init__(self, parent=None):
         super(About, self).__init__(parent)  # Initializing object
         uic.loadUi(aboutUi, self)  # Loading the main UI
-
-
-def get_bloomberg_palette():
-    palette = QPalette()
-    palette.setColor(QPalette.Window, QColor(53, 53, 53))
-    palette.setColor(QPalette.WindowText, QColor(255, 140, 0))
-    palette.setColor(QPalette.Base, QColor(25, 25, 25))
-    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-    palette.setColor(QPalette.ToolTipBase, QColor(0, 0, 0))
-    palette.setColor(QPalette.ToolTipText, QColor(255, 140, 0))
-    palette.setColor(QPalette.Text, QColor(255, 140, 0))
-    palette.setColor(QPalette.Button, QColor(53, 53, 53))
-    palette.setColor(QPalette.ButtonText, QColor(255, 140, 0))
-    palette.setColor(QPalette.BrightText, QColor(252, 0, 0))
-    palette.setColor(QPalette.Link, QColor(42, 130, 218))
-    palette.setColor(QPalette.Highlight, QColor(255, 140, 0))
-    palette.setColor(QPalette.HighlightedText, QColor(0, 0, 0))
-    return palette
-
-
-def get_yellow_palette():
-    palette = QPalette()
-    palette.setColor(QPalette.Window, QColor(53, 53, 53))
-    palette.setColor(QPalette.WindowText, QColor(255, 255, 0))  # change
-    palette.setColor(QPalette.Base, QColor(25, 25, 25))
-    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-    palette.setColor(QPalette.ToolTipBase, QColor(0, 0, 0))
-    palette.setColor(QPalette.ToolTipText, QColor(255, 255, 0))  # change
-    palette.setColor(QPalette.Text, QColor(255, 255, 0))  # change
-    palette.setColor(QPalette.Button, QColor(53, 53, 53))
-    palette.setColor(QPalette.ButtonText, QColor(255, 255, 0))  # change
-    palette.setColor(QPalette.BrightText, QColor(252, 0, 0))
-    palette.setColor(QPalette.Link, QColor(42, 130, 218))
-    palette.setColor(QPalette.Highlight, QColor(255, 255, 0))  # change
-    palette.setColor(QPalette.HighlightedText, QColor(0, 0, 0))
-    return palette
-
-
-def get_green_palette():
-    palette = QPalette()
-    palette.setColor(QPalette.WindowText, QColor(0, 255, 0))  # change
-    palette.setColor(QPalette.ToolTipText, QColor(0, 255, 0))  # change
-    palette.setColor(QPalette.Text, QColor(0, 255, 0))  # change
-    palette.setColor(QPalette.ButtonText, QColor(0, 255, 0))  # change
-    palette.setColor(QPalette.Highlight, QColor(0, 255, 0))  # change
-    return palette
-
-
-def get_red_palette():
-    palette = QPalette()
-    palette.setColor(QPalette.Window, QColor(53, 53, 53))
-    palette.setColor(QPalette.WindowText, QColor(255, 0, 0))  # change
-    palette.setColor(QPalette.Base, QColor(25, 25, 25))
-    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-    palette.setColor(QPalette.ToolTipBase, QColor(0, 0, 0))
-    palette.setColor(QPalette.ToolTipText, QColor(255, 0, 0))  # change
-    palette.setColor(QPalette.Text, QColor(255, 0, 0))  # change
-    palette.setColor(QPalette.Button, QColor(53, 53, 53))
-    palette.setColor(QPalette.ButtonText, QColor(255, 0, 0))  # change
-    palette.setColor(QPalette.BrightText, QColor(252, 0, 0))
-    palette.setColor(QPalette.Link, QColor(42, 130, 218))
-    palette.setColor(QPalette.Highlight, QColor(255, 0, 0))  # change
-    palette.setColor(QPalette.HighlightedText, QColor(0, 0, 0))
-    return palette
-
-
-def get_dark_palette():
-    palette = QPalette()
-    palette.setColor(QPalette.Window, QColor(53, 53, 53))
-    palette.setColor(QPalette.WindowText, QColor(255, 255, 255))
-    palette.setColor(QPalette.Base, QColor(25, 25, 25))
-    palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-    palette.setColor(QPalette.ToolTipBase, QColor(0, 0, 0))
-    palette.setColor(QPalette.ToolTipText, QColor(255, 255, 255))
-    palette.setColor(QPalette.Text, QColor(255, 255, 255))
-    palette.setColor(QPalette.Button, QColor(53, 53, 53))
-    palette.setColor(QPalette.ButtonText, QColor(255, 255, 255))
-    palette.setColor(QPalette.BrightText, QColor(255, 0, 0))
-    palette.setColor(QPalette.Link, QColor(42, 130, 218))
-    palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-    palette.setColor(QPalette.HighlightedText, QColor(0, 0, 0))
-    return palette
-
-
-def get_light_palette():
-    palette = QPalette()
-    return palette
 
 
 def main():
---
+++ algobot/threadWorkers.py
@@ -0,0 +1,51 @@
+import traceback
+
+from PyQt5.QtCore import QObject, QRunnable, pyqtSignal, pyqtSlot
+
+
+class WorkerSignals(QObject):
+    """
+    Defines the signals available from a running worker thread.
+    """
+    finished = pyqtSignal()
+    error = pyqtSignal(str)
+    result = pyqtSignal(object)
+    progress = pyqtSignal(int)
+
+
+class Worker(QRunnable):
+    """
+    Worker thread
+
+    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+
+    :param callback: The function callback to run on this worker thread. Supplied args and
+                     kwargs will be passed through to the runner.
+    :type callback: function
+    :param args: Arguments to pass to the callback function
+    :param kwargs: Keywords to pass to the callback function
+
+    """
+
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+    @pyqtSlot()
+    def run(self):
+        """
+        Initialise the runner function with passed args, kwargs.
+        """
+
+        # Retrieve args/kwargs here; and fire processing using them
+        try:
+            self.fn(*self.args, **self.kwargs)
+        except Exception as e:
+            print(f'Error: {e}')
+            traceback.print_exc()
+            self.signals.error.emit(str(e))
