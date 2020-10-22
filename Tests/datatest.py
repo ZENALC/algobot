80f9938758d3067e1c622885872613b36f8bfefb
Mihir Shrestha <mihir.xtha@gmail.com>
2020-10-21 21:46:30 -0400

    added datatest

---
+++ Tests/datatest.py
@@ -0,0 +1,26 @@
+import unittest
+from data import Data
+
+
+class MyTestCase(unittest.TestCase):
+    dataObject = Data(interval='1h', symbol='BTCUSDT', loadData=True)
+
+    def test_initialization(self):
+        self.assertTrue(self.dataObject.data, 'Data initialization test.')
+
+    def test_validate_interval(self):
+        self.dataObject.validate_interval('15m')
+        self.dataObject.validate_interval('30m')
+        self.assertRaises(ValueError, self.dataObject.validate_interval, '51m')
+
+    def test_validate_symbol(self):
+        self.dataObject.validate_symbol('BTCUSDT')
+        self.dataObject.validate_symbol('YFIUSDT')
+        self.assertRaises(ValueError, self.dataObject.validate_symbol, 'BAD')
+
+    def test_get_latest_row(self):
+        self.assertTrue(self.dataObject.get_latest_database_row())
+
+
+if __name__ == '__main__':
+    unittest.main()
