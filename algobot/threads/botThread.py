ddd41f2899664aa8140e65b18ca02dfe1353afc3
Mihir Shrestha <mihir.xtha@gmail.com>
2021-03-08 00:35:14 -0500

    refactoring

--- algobot/threads/botThread.py
+++ algobot/threads/botThread.py
@@ -79,7 +79,7 @@ class BotThread(QRunnable):
 
         if interval != '1m':
             lowerInterval = sortedIntervals[sortedIntervals.index(interval) - 1]
-            intervalString = helpers.convert_interval_to_string(lowerInterval)
+            intervalString = helpers.convert_small_interval(lowerInterval)
             self.lowerIntervalNotification = True
             self.signals.activity.emit(caller, f'Retrieving {symbol} data for {intervalString.lower()} intervals...')
 
@@ -111,7 +111,7 @@ class BotThread(QRunnable):
         symbol = configDict['ticker'].currentText()
         precision = configDict['precision'].value()
         prettyInterval = configDict['interval'].currentText()
-        interval = helpers.convert_interval(prettyInterval)
+        interval = helpers.convert_long_interval(prettyInterval)
 
         if caller == SIMULATION:
             startingBalance = gui.configuration.simulationStartingBalanceSpinBox.value()
