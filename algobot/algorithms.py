7976a36d07410ef6f51bedf93edaf4ae2737a74c
Mihir Shrestha <mihir.xtha@gmail.com>
2021-03-08 00:31:13 -0500

    modified algorithms to support ascending or descending data

--- algobot/algorithms.py
+++ algobot/algorithms.py
@@ -2,22 +2,30 @@ from typing import List, Tuple
 from helpers import get_data_from_parameter
 
 
-def get_wma(data: List[dict], prices: int, parameter: str) -> float:
+def get_wma(data: List[dict], prices: int, parameter: str, desc: bool = True) -> float:
     """
     Calculates the weighted moving average from data provided.
     The data is assumed to be in descending order - meaning newer dates are in the front of the list.
+    :param desc: Order data is in. If descending, this will be true, else false.
     :param data: Data to calculate weighted moving average.
     :param prices: Periods of data to get weighted moving average.
     :param parameter: Parameter from data dictionary with which to get the weighted moving average.
     :return: Weighted moving average.
     """
-    total = get_data_from_parameter(data=data[0], parameter=parameter) * prices
-    data = data[1:]
+    if desc:
+        total = get_data_from_parameter(data=data[0], parameter=parameter) * prices
+        data = data[1:]
 
-    index = 0
-    for x in range(prices - 1, 0, -1):
-        total += x * get_data_from_parameter(data=data[index], parameter=parameter)
-        index += 1
+        index = 0
+        for x in range(prices - 1, 0, -1):
+            total += x * get_data_from_parameter(data=data[index], parameter=parameter)
+            index += 1
+    else:
+        total = get_data_from_parameter(data=data[-1], parameter=parameter) * prices
+        data = data[:-1]
+
+        for index, data in enumerate(data, start=1):
+            total += index * get_data_from_parameter(data, parameter)
 
     divisor = prices * (prices + 1) / 2
     wma = total / divisor
@@ -27,20 +35,20 @@ def get_wma(data: List[dict], prices: int, parameter: str) -> float:
 def get_sma(data: List[dict], prices: int, parameter: str) -> float:
     """
     Calculates the simple moving average from data provided.
-    The data is assumed to be in descending order - meaning newer dates are in the front of the list.
     :param data: Data to calculate simple moving average.
     :param prices: Periods of data to get simple moving average.
     :param parameter: Parameter from data dictionary with which to get the simple moving average.
     :return: Simple moving average.
     """
-    sma = sum([get_data_from_parameter(data=period, parameter=parameter) for period in data]) / prices
-    return sma
+    return sum([get_data_from_parameter(data=period, parameter=parameter) for period in data]) / prices
 
 
-def get_ema(data: List[dict], prices: int, parameter: str, sma_prices: int, memo: dict = None) -> Tuple[float, dict]:
+def get_ema(data: List[dict], prices: int, parameter: str, sma_prices: int, memo: dict = None, desc: bool = True) -> \
+        Tuple[float, dict]:
     """
     Calculates the exponential moving average from data provided.
     The data is assumed to be in descending order - meaning newer dates are in the front of the list.
+    :param desc: Order data is in. If descending, this will be true, else false.
     :param data: Data to calculate exponential moving average.
     :param prices: Periods to data to get exponential moving average.
     :param parameter: Parameter from data dictionary with which to get the exponential moving average.
@@ -51,23 +59,31 @@ def get_ema(data: List[dict], prices: int, parameter: str, sma_prices: int, memo
     multiplier = 2 / (prices + 1)
 
     if memo and prices in memo and parameter in memo[prices]:
-        current_price = get_data_from_parameter(data[0], parameter)
-        if memo[prices][parameter][-1][1] == data[0]['date_utc']:
+        index = 0 if desc else -1
+        current_price = get_data_from_parameter(data[index], parameter)
+        if memo[prices][parameter][-1][1] == data[index]['date_utc']:
             previous_ema = memo[prices][parameter][-2][0]
             ema = current_price * multiplier + previous_ema * (1 - multiplier)
             memo[prices][parameter][-1][0] = ema
-        elif memo[prices][parameter][-1][1] < data[0]['date_utc']:
+        elif memo[prices][parameter][-1][1] < data[index]['date_utc']:
             previous_ema = memo[prices][parameter][-1][0]
             ema = current_price * multiplier + previous_ema * (1 - multiplier)
-            memo[prices][parameter].append([ema, data[0]['date_utc']])
+            memo[prices][parameter].append([ema, data[index]['date_utc']])
         else:
             raise ValueError("Something went wrong in the calculation of the EMA.")
     else:
-        sma_data = data[len(data) - sma_prices:]
-        ema = get_sma(sma_data, sma_prices, parameter)
-        values = [[ema, data[len(data) - sma_prices]['date_utc']]]
+        if desc:
+            sma_data = data[len(data) - sma_prices:]
+        else:
+            sma_data = data[:sma_prices]
 
-        data = data[:len(data) - sma_prices][::-1]  # Reverse the data to start from back to front.
+        ema = get_sma(sma_data, sma_prices, parameter)
+        if desc:
+            values = [[ema, data[len(data) - sma_prices]['date_utc']]]
+            data = data[:len(data) - sma_prices][::-1]  # Reverse the data to start from back to front.
+        else:
+            values = [[ema, data[sma_prices - 1]['date_utc']]]
+            data = data[sma_prices:]
 
         for period in data:
             current_price = get_data_from_parameter(period, parameter=parameter)
