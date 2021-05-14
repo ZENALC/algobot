0136ae3ae0c0d8cdd2b28f3901ba2dfd8ad9fdfb
Mihir Shrestha <mihir.xtha@gmail.com>
2021-05-13 23:59:12 -0400

    removed about.ui and populate about.py through code and added a version.txt file

--- algobot/interface/about.py
+++ algobot/interface/about.py
@@ -1,14 +1,38 @@
 import os
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog, QMainWindow
+from PyQt5.QtWidgets import QDialog, QHBoxLayout, QMainWindow, QPlainTextEdit
 
 from algobot.helpers import ROOT_DIR
-
-aboutUi = os.path.join(ROOT_DIR, 'UI', 'about.ui')
 
 
 class About(QDialog):
     def __init__(self, parent: QMainWindow = None):
-        super(About, self).__init__(parent)  # Initializing object
-        uic.loadUi(aboutUi, self)  # Loading the main UI
+        super(About, self).__init__(parent)
+        layout = QHBoxLayout()
+
+        version = self.get_version()
+        description = f"Algobot Version {version}\n\n" \
+                      f"Algobot is an open-source software written in Python that enables users to create automated " \
+                      f"bots that can trade, simulate, optimize, or backtest with strategies implemented.\n\n" \
+                      f"This program is not responsible for any financial burdens or debt incurred.\n\n" \
+                      f"Use at your own risk.\n\n" \
+                      f"For contribution, bug reports, or feature requests, visit https://github.com/ZENALC/algobot"
+
+        plain_text_edit = QPlainTextEdit()
+        plain_text_edit.setPlainText(description)
+        plain_text_edit.setReadOnly(True)
+        layout.addWidget(plain_text_edit)
+
+        self.setWindowTitle('About Algobot')
+        self.setLayout(layout)
+
+    @staticmethod
+    def get_version() -> str:
+        """
+        Reads version from version.txt and returns it.
+        """
+        version_file = os.path.join(ROOT_DIR, 'version.txt')
+        with open(version_file) as f:
+            version = f.read().strip()
+
+        return version
