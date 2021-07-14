fdd9e96be8c2825768cb42f78e6cfeb2bec4448f
Malachi Soord <inverse.chi@gmail.com>
2021-07-14 20:56:42 +0200

    Fix up tests

--- tests/test_enums.py
+++ tests/test_enums.py
@@ -4,14 +4,11 @@ from algobot.enums import OrderType
 
 
 class OrderTypeTest(unittest.TestCase):
-    def test_get_trailing_or_stop_loss_string(self):
+    def test_to_str(self):
         self.assertEqual(OrderType.to_str(OrderType.STOP), "Stop")
         self.assertEqual(OrderType.to_str(OrderType.TRAILING), "Trailing")
         self.assertEqual(OrderType.to_str(None), "None")
 
-
-class LossStrategyTest(unittest.TestCase):
-    def test_get_trailing_or_stop_loss_string(self):
-        self.assertEqual(OrderType.to_str(OrderType.STOP), "Stop")
-        self.assertEqual(OrderType.to_str(OrderType.TRAILING), "Trailing")
-        self.assertEqual(OrderType.to_str(None), "None")
+    def test_to_str_unsupported(self):
+        with self.assertRaises(ValueError):
+            OrderType.to_str(100)
