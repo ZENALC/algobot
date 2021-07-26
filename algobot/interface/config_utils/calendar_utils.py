2b9495dc8fe895cdd7bb3b4a532bdb5464022116
Mihir Shrestha <mihir.xtha@gmail.com>
2021-07-25 20:53:01 -0400

    change to snake case +2

--- algobot/interface/config_utils/calendar_utils.py
+++ algobot/interface/config_utils/calendar_utils.py
@@ -3,7 +3,7 @@ Calendar helper functions for configuration.py can be found here.
 """
 
 import datetime
-from typing import Tuple
+from typing import Optional, Tuple
 
 from dateutil import parser
 from PyQt5.QtCore import QDate
@@ -11,18 +11,18 @@ from PyQt5.QtCore import QDate
 from algobot.enums import BACKTEST
 
 
-def get_calendar_dates(config_obj, caller: int = BACKTEST) -> Tuple[datetime.date or None, datetime.date or None]:
+def get_calendar_dates(config_obj, caller: int = BACKTEST) -> Tuple[Optional[datetime.date], Optional[datetime.date]]:
     """
     Returns start end end dates for backtest. If both are the same, returns None.
     :param config_obj: Configuration QDialog object (from configuration.py)
     :param caller: Caller object that called this function.
     :return: Start and end dates for backtest.
     """
-    startDate = config_obj.optimizer_backtest_dict[caller]['startDate'].selectedDate().toPyDate()
-    endDate = config_obj.optimizer_backtest_dict[caller]['endDate'].selectedDate().toPyDate()
-    if startDate == endDate:
+    start_date = config_obj.optimizer_backtest_dict[caller]['startDate'].selectedDate().toPyDate()
+    end_date = config_obj.optimizer_backtest_dict[caller]['endDate'].selectedDate().toPyDate()
+    if start_date == end_date:
         return None, None
-    return startDate, endDate
+    return start_date, end_date
 
 
 def setup_calendar(config_obj, caller: int = BACKTEST):
@@ -33,25 +33,25 @@ def setup_calendar(config_obj, caller: int = BACKTEST):
     """
     data = config_obj.optimizer_backtest_dict[caller]['data']
     if isinstance(data[0]['date_utc'], str):
-        startDate = parser.parse(data[0]['date_utc'])
-        endDate = parser.parse(data[-1]['date_utc'])
+        start_date = parser.parse(data[0]['date_utc'])
+        end_date = parser.parse(data[-1]['date_utc'])
     else:
-        startDate = data[0]['date_utc']
-        endDate = data[-1]['date_utc']
+        start_date = data[0]['date_utc']
+        end_date = data[-1]['date_utc']
 
-    if startDate > endDate:
-        startDate, endDate = endDate, startDate
+    if start_date > end_date:
+        start_date, end_date = end_date, start_date
 
-    startYear, startMonth, startDay = startDate.year, startDate.month, startDate.day
-    qStartDate = QDate(startYear, startMonth, startDay)
+    start_year, start_month, start_day = start_date.year, start_date.month, start_date.day
+    q_start_date = QDate(start_year, start_month, start_day)
 
-    endYear, endMonth, endDay = endDate.year, endDate.month, endDate.day
-    qEndDate = QDate(endYear, endMonth, endDay)
+    end_year, end_month, end_day = end_date.year, end_date.month, end_date.day
+    q_end_date = QDate(end_year, end_month, end_day)
 
     config_obj.optimizer_backtest_dict[caller]['startDate'].setEnabled(True)
-    config_obj.optimizer_backtest_dict[caller]['startDate'].setDateRange(qStartDate, qEndDate)
-    config_obj.optimizer_backtest_dict[caller]['startDate'].setSelectedDate(qStartDate)
+    config_obj.optimizer_backtest_dict[caller]['startDate'].setDateRange(q_start_date, q_end_date)
+    config_obj.optimizer_backtest_dict[caller]['startDate'].setSelectedDate(q_start_date)
 
     config_obj.optimizer_backtest_dict[caller]['endDate'].setEnabled(True)
-    config_obj.optimizer_backtest_dict[caller]['endDate'].setDateRange(qStartDate, qEndDate)
-    config_obj.optimizer_backtest_dict[caller]['endDate'].setSelectedDate(qEndDate)
+    config_obj.optimizer_backtest_dict[caller]['endDate'].setDateRange(q_start_date, q_end_date)
+    config_obj.optimizer_backtest_dict[caller]['endDate'].setSelectedDate(q_end_date)
