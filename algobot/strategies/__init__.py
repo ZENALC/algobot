4c03ce28349faa4ed037d375c0127ee60a6da302
Mihir Shrestha <mihir.xtha@gmail.com>
2021-08-15 21:22:03 -0400

    reformat

--- algobot/strategies/__init__.py
+++ algobot/strategies/__init__.py
@@ -20,71 +20,51 @@ MA_STREAM_MAP: Dict[str, Dict[str, Union[Callable, List[namedtuple]]]] = {
     'DEMA': {  # TESTED
         STREAM: lambda s, i: talib.DEMA(s, i).iloc[-1],
         TALIB: talib.DEMA,
-        ARGS: [
-            ARGUMENT("Time period", int)
-        ]
+        ARGS: [ARGUMENT("Time period", int)]
     },
     'EMA': {  # TESTED
         STREAM: lambda s, i: talib.EMA(s, i).iloc[-1],
         TALIB: talib.EMA,
-        ARGS: [
-            ARGUMENT("Time period", int)
-        ]
+        ARGS: [ARGUMENT("Time period", int)]
     },
     'FAMA': {
         STREAM: lambda s, i, fast, slow: talib.MAMA(s, i, fast, slow).iloc[-1][1],  # 0th is MAMA, 1st is FAMA.
         TALIB: talib.MAMA,
-        ARGS: [
-            ARGUMENT("Time period", int), ARGUMENT("Fast Limit", int), ARGUMENT("Slow Limit", int)
-        ]
+        ARGS: [ARGUMENT("Time period", int), ARGUMENT("Fast Limit", int), ARGUMENT("Slow Limit", int)]
     },
     'KAMA': {  # TESTED
         STREAM: lambda s, i: talib.KAMA(s, i).iloc[-1],
         TALIB: talib.KAMA,
-        ARGS: [
-            ARGUMENT("Time period", int)
-        ]
+        ARGS: [ARGUMENT("Time period", int)]
     },
     'MAMA': {
         STREAM: lambda s, i, fast, slow: talib.MAMA(s, i, fast, slow).iloc[-1][0],  # 0th is MAMA, 1st is FAMA.
         TALIB: talib.MAMA,
-        ARGS: [
-            ARGUMENT("Time period", int), ARGUMENT("Fast Limit", int), ARGUMENT("Slow Limit", int)
-        ]
+        ARGS: [ARGUMENT("Time period", int), ARGUMENT("Fast Limit", int), ARGUMENT("Slow Limit", int)]
     },
     'SMA': {  # TESTED
         STREAM: talib.stream.SMA,
         TALIB: talib.SMA,
-        ARGS: [
-            ARGUMENT("Time period", int)
-        ]
+        ARGS: [ARGUMENT("Time period", int)]
     },
     'TEMA': {  # TESTED
         STREAM: lambda s, i: talib.TEMA(s, i).iloc[-1],
         TALIB: talib.TEMA,
-        ARGS: [
-            ARGUMENT("Time period", int)
-        ]
+        ARGS: [ARGUMENT("Time period", int)]
     },
     'TRIMA': {  # TESTED
         'stream': lambda s, i: talib.TRIMA(s, i).iloc[-1],
         'talib': talib.TRIMA,
-        'args': [
-            ARGUMENT("Time period", int)
-        ]
+        'args': [ARGUMENT("Time period", int)]
     },
     'T3': {
         STREAM: lambda s, i, v: talib.T3(s, i, v).iloc[-1],
         TALIB: talib.T3,
-        ARGS: [
-            ARGUMENT("Time period", int), ARGUMENT("V Factor", float)
-        ]
+        ARGS: [ARGUMENT("Time period", int), ARGUMENT("V Factor", float)]
     },
     'WMA': {  # TESTED
         STREAM: talib.stream.WMA,
         TALIB: talib.WMA,
-        ARGS: [
-            ARGUMENT("Time period", int)
-        ]
+        ARGS: [ARGUMENT("Time period", int)]
     },
 }
