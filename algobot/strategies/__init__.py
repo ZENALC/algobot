715e0f20df2b3568ce9969291f18e366b28f69f3
Mihir Shrestha <mihir.xtha@gmail.com>
2021-11-10 08:40:24 -0500

    bandaid

--- algobot/strategies/__init__.py
+++ algobot/strategies/__init__.py
@@ -9,8 +9,9 @@ from typing import Any, Callable, List, Type
 
 import talib
 
-# This is importing all strategies within the current working directory.
-__all__ = [basename(f)[:-3] for f in listdir(dirname(__file__)) if f[-3:] == ".py" and not f.endswith("__init__.py")]
+# This is importing all strategies within the current working directory. We ignore __init__ and custom.py
+__all__ = [basename(f)[:-3] for f in listdir(dirname(__file__)) if f[-3:] == ".py"
+           and not f.endswith("__init__.py") and not f == 'custom.py']
 
 
 @dataclass
