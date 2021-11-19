5013b2d2ead90cd7469480a71328cf77fb76babf
Mihir Shrestha <mihir.xtha@gmail.com>
2021-11-19 12:05:39 -0500

    ISORT (#140)

--- algobot/interface/utils.py
+++ algobot/interface/utils.py
@@ -3,11 +3,115 @@ File containing utility functions for the GUI.
 """
 
 from datetime import datetime
-from typing import List
+from typing import List, Union
 
+import talib
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QComboBox, QDialog, QMessageBox, QSizePolicy, QSpacerItem, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import (QComboBox, QDialog, QDoubleSpinBox, QLineEdit, QMessageBox, QSizePolicy, QSpacerItem,
+                             QSpinBox, QTableWidget, QTableWidgetItem)
+
+from algobot.interface.configuration_helpers import get_default_widget
+
+# TALIB sets moving averages by numbers. This is not very appealing in the frontend, so we'll map it to its
+#  appropriate moving average.
+MOVING_AVERAGE_TYPES_BY_NUM = vars(talib.MA_Type)['_lookup']
+MOVING_AVERAGE_TYPES_BY_NAME = {v: k for k, v in MOVING_AVERAGE_TYPES_BY_NUM.items()}
+MOVING_AVERAGES_LIST = list(MOVING_AVERAGE_TYPES_BY_NAME.keys())
+
+OPERATORS = ['>', '<', '>=', '<=', '==', '!=']
+
+# Mappings from TALIB parameters to better display names.
+PARAMETER_MAP = {
+    'acceleration': 'Acceleration',
+    'accelerationinitlong': 'Acceleration Init Long',
+    'accelerationinitshort': 'Acceleration Init Short',
+    'accelerationlong': 'Acceleration Long',
+    'accelerationmaxlong': 'Acceleration Max Long',
+    'accelerationmaxshort': 'Acceleration Max Short',
+    'accelerationshort': "Acceleration Short",
+    'fastd_matype': "Fast D MA Type",
+    'fastd_period': "Fast D Period",
+    'fastk_period': "Fast K Period",  # noqa
+    'fastlimit': "Fast Limit",  # noqa
+    'fastmatype': "Fast MA Type",
+    'fastperiod': 'Fast Period',
+    'matype': "MA Type",
+    'maximum': "Maximum",
+    'maxperiod': "Max Period",  # noqa
+    'minperiod': 'Min Period',  # noqa
+    'nbdev': 'NB Dev',  # noqa
+    'nbdevdn': 'NB Dev Down',
+    'nbdevup': 'NB Dev Up',
+    'offsetonreverse': "Offset On Reverse",  # noqa
+    'penetration': 'Penetration',
+    'signalmatype': 'Signal MA Type',
+    'signalperiod': 'Signal Period',  # noqa
+    'slowd_matype': 'Slow D MA Type',  # noqa
+    'slowd_period': 'Slow D Period',  # noqa
+    'slowk_matype': 'Slow K MA Type',  # noqa
+    'slowk_period': 'Slow K Period',  # noqa
+    'slowlimit': "Slow Limit",  # noqa
+    'slowmatype': "Slow MA Type",
+    'slowperiod': "Slow Period",
+    'startvalue': 'Start Value',  # noqa
+    'timeperiod': "Time Period",
+    'timeperiod1': "Time Period 1",
+    'timeperiod2': 'Time Period 2',
+    'timeperiod3': "Time Period 3",
+    'vfactor': 'V Factor'
+}
+
+
+def get_param_obj(default_value: Union[float, int, str], param_name: str):
+    """
+    Get param widget based on param and param_name provided.
+    :param default_value: Param type.
+    :param param_name: Name of the parameter (e.g. matype)
+    :return: Parameter object.
+    """
+    if isinstance(default_value, int):
+        # TALIB stores MA types as ints. So, we must see what that num maps to.
+        if 'matype' in param_name:
+            default_moving_average = MOVING_AVERAGE_TYPES_BY_NUM[default_value]
+
+            input_obj = QComboBox()
+            input_obj.addItems(MOVING_AVERAGES_LIST)
+            input_obj.setCurrentIndex(MOVING_AVERAGES_LIST.index(default_moving_average))
+            return input_obj
+
+        # Annoying edge case where TALIB expects a float even though it should accept an int.
+        elif param_name.lower() in {'nbdevdn', 'nbdevup'}:
+            return get_default_widget(QDoubleSpinBox, default_value, None, None)
+
+        else:
+            return get_default_widget(QSpinBox, default_value, None, None)
+
+    elif param_name == 'price':
+        input_obj = QComboBox()
+        input_obj.addItems(['Open', 'High', 'Low', 'Close', 'Open/Close', 'High/Low'])
+        return input_obj
+
+    elif isinstance(default_value, float):
+        return get_default_widget(QDoubleSpinBox, default_value, None, None)
+
+    elif isinstance(default_value, str):
+        return QLineEdit()
+
+    else:
+        raise ValueError("Unknown type of data encountered.")
+
+
+def confirm_message_box(message: str, parent) -> bool:
+    """
+    Show a confirmation message box and return true if confirmed else false.
+    :param message: Message to show.
+    :param parent: Parent to show message on top of.
+    :return: Boolean whether user has confirmed or not.
+    """
+    msg_box = QMessageBox
+    ret = msg_box.question(parent, 'Warning', message, msg_box.Yes | msg_box.No)
+    return ret == msg_box.Yes
 
 
 def get_bold_font() -> QFont:
